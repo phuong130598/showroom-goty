2c618a310ab78250f3d191620b8f34261816f85b
Nguyen Ky Phuong (s117326) <phuong.nguyenky@student.ap.be>
2022-11-27 14:38:36 +0100

    games page added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Showroom: Games of the Year`,
+    description: `This website will show you the games that won the 'Game of the Year' award for each year.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
-    `gatsby-plugin-image`,
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-wordpress",
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        url: "http://showroom-goty.local/graphql",
       },
     },
   ],
---
+++ src/pages/games.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+import Seo from "../components/seo"
+
+const GamesPage = () =>
+{
+    return (
+        <Layout>
+            <Seo title="Games" />
+            <h2>All the GOTY winners</h2>
+        </Layout>
+    )
+}
+
+
+export default GamesPage;
